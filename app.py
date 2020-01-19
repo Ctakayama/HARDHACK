fd0b92b4aced2881af86f5b0f42112b9a244af23
Wells Huang <wch008@ucsd.edu>
2020-01-18 17:50:34 -0800

    ss

--- app.py
+++ app.py
@@ -14,41 +14,58 @@ https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_a
 """
 
 note = cv2.imread('note.jpg')
-w_resize = note.shape[1]*0.4
-h_resize = note.shape[0]*0.2
+w_resize = note.shape[1]*0.23
+h_resize = note.shape[0]*0.23
 
 note = cv2.resize(note, (int(w_resize), int(h_resize)))
 
 # This function changes labels to text string
 def labelToString(dic, predictions):
     for p in predictions:
-        if p.label in dic:
+        if p.label in dic.keys():
             p.label = dic.get(p.label)
     return predictions
 
 # This function adds notes to image
-def addNotes(image, predictions, note):
-    boxes = [p.box for p in predictions]
-    for b in boxes:
-        image = overlayNote(image, note, b)
+def addNotes(image, predictions):
+    for p in predictions:
+        image = overlayNote(image, p)
     return image
 
 # This function addes single note to image
-def overlayNote(image, note, box):
+def overlayNote(image, p):
+    box = p.box
     x = box.end_x
     y = box.start_y
     #if image.shape[1] - y > note.shape[1] and image.shape[0] - x > note.shape[0]:
-    image[y:y + note.shape[0], x - note.shape[1]:x] = note
+    xi = 10
+    yi = 10
+    temp = note[:,:,:]
+    for s in strToList(p.label):
+        cv2.putText(temp, s, (xi,yi), cv2.FONT_HERSHEY_PLAIN, 0.7, (255,0,0), 1, cv2.LINE_AA)
+        yi = yi + 20
+    count = 0
+    try:
+        image[y:y + temp.shape[0], x - temp.shape[1]:x] = temp
+    except:
+        count = count + 1
+        print('failed')
+        print(count)
     return image
-
+def strToList (s):
+    return s.split(' ')
 
 def main():
 
-    label_defs = {"person" : "test"}
+    label_defs = {"person" : "SPREAD KINDNESS",
+                  "chair" : "CAREFUL SITTING DOWN",
+                  "bottle" : "STAY HYDRATED !!!",
+                  "tvmonitor" : "GO OUTSIDE"}
 
     obj_detect = edgeiq.ObjectDetection(
             "alwaysai/mobilenet_ssd")
     obj_detect.load(engine=edgeiq.Engine.DNN)
+
 
     print("Loaded model:\n{}\n".format(obj_detect.model_id))
     print("Engine: {}".format(obj_detect.engine))
@@ -58,7 +75,7 @@ def main():
     fps = edgeiq.FPS()
 
     try:
-        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
+        with edgeiq.WebcamVideoStream(cam=1) as video_stream, \
                 edgeiq.Streamer() as streamer:
             # Allow Webcam to warm up
             time.sleep(2.0)
@@ -66,20 +83,21 @@ def main():
 
             # loop detection
             while True:
+
                 frame = video_stream.read()
                 results = obj_detect.detect_objects(frame, confidence_level=.5)
                 frame = edgeiq.markup_image(
-                        frame, labelToString(label_defs, results.predictions), show_labels = True,
+                        frame, labelToString(label_defs, results.predictions), show_labels = False,
                         show_confidences = False, colors=obj_detect.colors, 
                         line_thickness = 0)
-                frame = addNotes(frame, results.predictions, note)
+                frame = addNotes(frame, results.predictions)
 
                 # Generate text to display on streamer
                 text = ["Model: {}".format(obj_detect.model_id)]
                 text.append(
                         "Inference time: {:1.3f} s".format(results.duration))
                 text.append("Objects:")
-
+                text.append("fps:{:2.2f}".format(fps.compute_fps()))
                 for prediction in results.predictions:
                     text.append("{}: {:2.2f}%".format(
                         prediction.label, prediction.confidence * 100))
